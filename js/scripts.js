b6c8b458e96ed492cb7202cabe778518314d6ff4
Sana Zahlan <sanazahlan@yahoo.com>
2022-04-10 01:09:45 -0700

    Update code to hide and show language suggestion.

--- js/scripts.js
+++ js/scripts.js
@@ -8,13 +8,15 @@ $(document).ready(function() {
     const animal = $("#animal").val();
 
     if(phone === "Android" && science === "Physics" && color === "Red" && food === "Hot Dogs" && animal === "Cats") {
-      $("#output").text("You should learn C#!");
+      $(".language").text("C#");
     } else if (phone === "Apple" && science === "Biology" && color === "Blue" && food === "Cheese Burgers" && animal === "Dogs") {
-      $("#output").text("You shoud learn Python!");
+      $(".language").text("Python");
     } else if (phone === "Microsoft" && science === "Chemistry" && color === "Green" && food === "Pizza" && animal === "Guinea Pigs") {
-      $("#output").text("You should learn Ruby!");
+      $(".language").text("Ruby");
     } else {
-      $("#output").text("You should learn JavaScript!");
+      $(".language").text("JavaScript");
     }
+
+    $("#output").show();
   });  
 });
