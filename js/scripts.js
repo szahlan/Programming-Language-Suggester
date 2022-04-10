2df7ce1182baabc47353253a6caf3e79e3163d64
Sana Zahlan <sanazahlan@yahoo.com>
2022-04-10 00:52:08 -0700

    Add branching to give languuage suggestion based on user input.

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,16 @@ $(document).ready(function() {
     const science = $("#science").val();
     const color = $("#color").val();
     const food = $("#food").val();
-    const animal = $("animal").val();
+    const animal = $("#animal").val();
+
+    if(phone === "Android" && science === "Physics" && color === "Red" && food === "Hot Dogs" && animal === "Cats") {
+      $("#output").text("You should learn C#!");
+    } else if (phone === "Apple" && science === "Biology" && color === "Blue" && food === "Cheese Burgers" && animal === "Dogs") {
+      $("#output").text("You shoud learn Python!");
+    } else if (phone === "Microsoft" && science === "Chemistry" && color === "Green" && food === "Pizza" && animal === "Guinea Pigs") {
+      $("#output").text("You should learn Ruby!");
+    } else {
+      $("#output").text("You should learn JavaScript!");
+    }
   });  
 });
